a15926a73ab188713266b91d8150da4042e790c6
jvuki <josipvukivuksan@gmail.com>
2025-03-28 15:02:32 +0100

    Add image to site

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Mail, Phone } from "lucide-react";
+import work from "./assets/images/working.jpg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center gap-10">
+    <div className="flex flex-col items-center justify-center gap-10 p-5">
       <h1 className="text-2xl">Cleaning Freak coming soon...</h1>
       <div className="flex flex-row gap-5">
         <Mail />
@@ -17,9 +18,9 @@ function App() {
       </div>
       <div className="flex flex-row gap-5">
         <Phone />
-
         <a href="tel:+385 95 555 9119">+385 95 555 9119</a>
       </div>
+      <img className="aspect-16/9 object-contain w-full" src={work} />
     </div>
   );
 }

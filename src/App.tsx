52b2fd511de634ad5a3427f80efb0be7661ab842
jvuki <josipvukivuksan@gmail.com>
2025-03-28 14:57:20 +0100

    Add basic info

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Mail, Phone } from "lucide-react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+    <div className="flex flex-col items-center justify-center gap-10">
+      <h1 className="text-2xl">Cleaning Freak coming soon...</h1>
+      <div className="flex flex-row gap-5">
+        <Mail />
+        <a
+          href="mailto:cleaningfreak.hr@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          cleaningfreak.hr@gmail.com
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="flex flex-row gap-5">
+        <Phone />
+
+        <a href="tel:+385 95 555 9119">+385 95 555 9119</a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
